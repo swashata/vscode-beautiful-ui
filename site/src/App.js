77498d475f6f0448f3dd51b835d66cbe233b45e9
Swashata Ghosh <swashata@wpquark.com>
2018-05-08 21:04:39 +0530

    feat: bootstrap docs site

--- site/src/App.js
+++ site/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './bui-logo.png';
+import Demo from './components/Demo';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">βui - Beautiful UI for VSCode.</h1>
+				</header>
+				<p className="App-intro">
+					βui is a set of 27 themes ported from <a href="https://github.com/ihodev/sublime-da-cs">Sublime DA CS</a>.
+				</p>
+				<div className="app-demo">
+					<Demo />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
